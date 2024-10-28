597c97e3aba3ef1ec6bc9407fe6144244268d75c
khaihc <hoangkhaikute1508@gmail.com>
2024-10-28 16:49:08 +0700

    chore: add try catch for all service

--- src/services/productService.ts
+++ src/services/productService.ts
@@ -5,86 +5,116 @@ import { productQueries } from '../queries/productQueries.sql';
 export class ProductService {
     
     async fetchAllProducts(): Promise<Product[]> {
-        return await executeQuery(async (connection) => {
-            const result = await connection.query(productQueries.SELECT_ALL_PRODUCTS);
-            if (result.rows.length > 0) {
-                return result.rows;
-            } else {
-                throw new Error(`No products found in the database.`);
-            }
-        });
+        try {
+            return await executeQuery(async (connection) => {
+                const result = await connection.query(productQueries.SELECT_ALL_PRODUCTS);
+                if (result.rows.length > 0) {
+                    return result.rows;
+                } else {
+                    throw new Error(`No products found in the database.`);
+                }
+            });
+        } catch (error) {
+            console.error('Error fetching all products:', error);
+            throw new Error('Could not fetch products.');
+        }
     }
 
     async fetchProductInformation(pr_id: number): Promise<Product[]> {
-        return await executeQuery(async (connection) => {
-            const result = await connection.query(productQueries.SELECT_PRODUCT_BY_ID, [pr_id]);
-            if (result.rows.length > 0) {
-                return result.rows;
-            } else {
-                throw new Error(`Product with ID ${pr_id} does not exist.`);
-            }
-        });
+        try {
+            return await executeQuery(async (connection) => {
+                const result = await connection.query(productQueries.SELECT_PRODUCT_BY_ID, [pr_id]);
+                if (result.rows.length > 0) {
+                    return result.rows;
+                } else {
+                    throw new Error(`Product with ID ${pr_id} does not exist.`);
+                }
+            });
+        } catch (error) {
+            console.error('Error fetching product information:', error);
+            throw new Error('Could not fetch product information.');
+        }
     }
 
     async saveProduct(product: Product): Promise<Product> {
-        return await executeQuery(async (connection) => {
-            // Check if the product name already exists
-            const { rows } = await connection.query(productQueries.CHECK_PRODUCT_EXISTENCE, [product.name]);
+        try {
+            return await executeQuery(async (connection) => {
+                // Check if the product name already exists
+                const { rows } = await connection.query(productQueries.CHECK_PRODUCT_EXISTENCE, [product.name]);
 
-            if (rows[0].exists) {
-                throw new Error("Product name already taken. Please choose another.");
-            }
+                if (rows[0].exists) {
+                    throw new Error("Product name already taken. Please choose another.");
+                }
 
-            // Insert the new product without specifying the ID
-            const result = await connection.query(productQueries.INSERT_PRODUCT, [product.name, product.price, product.category]);
+                // Insert the new product without specifying the ID
+                const result = await connection.query(productQueries.INSERT_PRODUCT, [product.name, product.price, product.category]);
 
-            return result.rows[0];
-        });
+                return result.rows[0];
+            });
+        } catch (error) {
+            console.error('Error saving product:', error);
+            throw new Error('Could not save product.');
+        }
     }
 
     async deleteProduct(prId: number): Promise<string> {
-        return await executeQuery(async (connection) => {
-            // Check if the product exists
-            const existingProduct = await connection.query(productQueries.SELECT_PRODUCT_BY_ID, [prId]);
+        try {
+            return await executeQuery(async (connection) => {
+                // Check if the product exists
+                const existingProduct = await connection.query(productQueries.SELECT_PRODUCT_BY_ID, [prId]);
 
-            if (existingProduct.rows.length === 0) {
-                throw new Error("Product not found. Unable to delete.");
-            }
+                if (existingProduct.rows.length === 0) {
+                    throw new Error("Product not found. Unable to delete.");
+                }
 
-            // Delete the product
-            await connection.query(productQueries.DELETE_PRODUCT, [prId]);
+                // Delete the product
+                await connection.query(productQueries.DELETE_PRODUCT, [prId]);
 
-            return `Product with ID ${prId} has been successfully deleted.`;
-        });
+                return `Product with ID ${prId} has been successfully deleted.`;
+            });
+        } catch (error) {
+            console.error('Error deleting product:', error);
+            throw new Error('Could not delete product.');
+        }
     }
 
     async updateProduct(prId: number, updatedData: Partial<Product>): Promise<{ data: Product; message: string }> {
-        return await executeQuery(async (connection) => {
-            // Check if the product exists
-            const existingProduct = await connection.query(productQueries.SELECT_PRODUCT_BY_ID, [prId]);
+        try {
+            return await executeQuery(async (connection) => {
+                // Check if the product exists
+                const existingProduct = await connection.query(productQueries.SELECT_PRODUCT_BY_ID, [prId]);
 
-            if (existingProduct.rows.length === 0) {
-                throw new Error("Product not found. Update operation cannot be completed.");
-            }
+                if (existingProduct.rows.length === 0) {
+                    throw new Error("Product not found. Update operation cannot be completed.");
+                }
 
-            const setClause = Object.keys(updatedData).map((key, index) => `${key} = $${index + 1}`).join(", ");
-            const sqlUpdate = productQueries.UPDATE_PRODUCT(setClause);
-            
-            const values = [...Object.values(updatedData), prId];
-            const result = await connection.query(sqlUpdate, values);
-            const updatedProduct = result.rows[0];
+                const setClause = Object.keys(updatedData).map((key, index) => `${key} = $${index + 1}`).join(", ");
+                const sqlUpdate = productQueries.UPDATE_PRODUCT(setClause);
+                
+                const values = [...Object.values(updatedData), prId];
+                const result = await connection.query(sqlUpdate, values);
+                const updatedProduct = result.rows[0];
 
-            return {
-                data: updatedProduct,
-                message: `Product with ID ${prId} has been successfully updated.`,
-            };
-        });
+                return {
+                    data: updatedProduct,
+                    message: `Product with ID ${prId} has been successfully updated.`,
+                };
+            });
+        } catch (error) {
+            console.error('Error updating product:', error);
+            throw new Error('Could not update product.');
+        }
     }
 
     async getProductsByCategory(category: string): Promise<Product[]> {
-        return await executeQuery(async (connection) => {
-            const { rows } = await connection.query(productQueries.SELECT_BY_CATEGORY, [category]);
-            return rows;
-        });
+        try {
+            return await executeQuery(async (connection) => {
+                const { rows } = await connection.query(productQueries.SELECT_BY_CATEGORY, [category]);
+                return rows;
+            });
+        } catch (error) {
+            console.error('Error fetching products by category:', error);
+            throw new Error('Could not fetch products by category.');
+        }
     }
 }
--- src/services/userService.ts
+++ src/services/userService.ts
@@ -11,86 +11,111 @@ const secret = String(process.env.SECRET_TOKEN);
 
 export class UserService {
     async fetchAllUsers(): Promise<User[]> {
-        return await executeQuery(async (connection) => {
-            const result = await connection.query(userQueries.SELECT_ALL_USERS);
-            if (result.rows.length > 0) {
-                return result.rows;
-            } else {
-                throw new Error(`No users found in the database.`);
-            }
-        });
+        try {
+            return await executeQuery(async (connection) => {
+                const result = await connection.query(userQueries.SELECT_ALL_USERS);
+                if (result.rows.length > 0) {
+                    return result.rows;
+                } else {
+                    throw new Error(`No users found in the database.`);
+                }
+            });
+        } catch (error) {
+            console.error('Error fetching all users:', error);
+            throw new Error('Could not fetch users.');
+        }
     }
 
     async fetchUserInformation(user_id: number): Promise<{ user: User; token: string }> {
-        return await executeQuery(async (connection) => {
-            const result = await connection.query(userQueries.SELECT_USER_BY_ID, [user_id]);
-            if (result.rows && result.rows.length > 0) {
-                const user = result.rows[0];
-                const token = jwt.sign({ userName: user.user_name, id: user.id }, secret);
-                return {
-                    user: user,
-                    token: token
-                };
-            } else {
-                throw new Error(`User with ID ${user_id} does not exist.`);
-            }
-        });
+        try {
+            return await executeQuery(async (connection) => {
+                const result = await connection.query(userQueries.SELECT_USER_BY_ID, [user_id]);
+                if (result.rows && result.rows.length > 0) {
+                    const user = result.rows[0];
+                    const token = jwt.sign({ userName: user.user_name, id: user.id }, secret);
+                    return {
+                        user: user,
+                        token: token
+                    };
+                } else {
+                    throw new Error(`User with ID ${user_id} does not exist.`);
+                }
+            });
+        } catch (error) {
+            console.error('Error fetching user information:', error);
+            throw new Error('Could not fetch user information.');
+        }
     }
 
     async registerUser(user: User): Promise<{ data: User; token: string }> {
-        return await executeQuery(async (connection) => {
-            // Check if the user_name already exists
-            const { rows } = await connection.query(userQueries.CHECK_USER_EXISTENCE, [user.user_name]);
-            if (rows[0].exists) {
-                throw new Error("User name already taken. Please choose another one.");
-            }
+        try {
+            return await executeQuery(async (connection) => {
+                // Check if the user_name already exists
+                const { rows } = await connection.query(userQueries.CHECK_USER_EXISTENCE, [user.user_name]);
+                if (rows[0].exists) {
+                    throw new Error("User name already taken. Please choose another one.");
+                }
 
-            // Insert the new user
-            const hashedPassword = bcrypt.hashSync(user.password + pepper, saltRound);
-            const result = await connection.query(userQueries.INSERT_USER, [user.first_name, user.last_name, user.user_name, hashedPassword]);
-            const createdUser = result.rows[0];
-            const token = jwt.sign({ userName: createdUser.user_name, id: createdUser.id }, secret);
-            
-            return {
-                data: createdUser,
-                token
-            };
-        });
+                // Insert the new user
+                const hashedPassword = bcrypt.hashSync(user.password + pepper, saltRound);
+                const result = await connection.query(userQueries.INSERT_USER, [user.first_name, user.last_name, user.user_name, hashedPassword]);
+                const createdUser = result.rows[0];
+                const token = jwt.sign({ userName: createdUser.user_name, id: createdUser.id }, secret);
+                
+                return {
+                    data: createdUser,
+                    token
+                };
+            });
+        } catch (error) {
+            console.error('Error registering user:', error);
+            throw new Error('Could not register user.');
+        }
     }
 
     async deleteUser(userId: number): Promise<string> {
-        return await executeQuery(async (connection) => {
-            // Check if the user exists
-            const existingUser = await connection.query(userQueries.SELECT_USER_BY_ID, [userId]);
-            if (existingUser.rows.length === 0) {
-                throw new Error("User not found. Unable to delete.");
-            }
+        try {
+            return await executeQuery(async (connection) => {
+                // Check if the user exists
+                const existingUser = await connection.query(userQueries.SELECT_USER_BY_ID, [userId]);
+                if (existingUser.rows.length === 0) {
+                    throw new Error("User not found. Unable to delete.");
+                }
 
-            // Delete the user
-            await connection.query(userQueries.DELETE_USER, [userId]);
-            return `User with ID ${userId} has been successfully deleted.`;
-        });
+                // Delete the user
+                await connection.query(userQueries.DELETE_USER, [userId]);
+                return `User with ID ${userId} has been successfully deleted.`;
+            });
+        } catch (error) {
+            console.error('Error deleting user:', error);
+            throw new Error('Could not delete user.');
+        }
     }
 
     async updateUser(userId: number, updatedData: Partial<User>): Promise<{ data: User; message: string }> {
-        return await executeQuery(async (connection) => {
-            // Check if the user exists
-            const existingUser = await connection.query(userQueries.SELECT_USER_BY_ID, [userId]);
-            if (existingUser.rows.length === 0) {
-                throw new Error("User not found. Update operation cannot be completed.");
-            }
+        try {
+            return await executeQuery(async (connection) => {
+                // Check if the user exists
+                const existingUser = await connection.query(userQueries.SELECT_USER_BY_ID, [userId]);
+                if (existingUser.rows.length === 0) {
+                    throw new Error("User not found. Update operation cannot be completed.");
+                }
 
-            const setClause = Object.keys(updatedData).map((key, index) => `${key} = $${index + 1}`).join(", ");
-            const sqlUpdate = userQueries.UPDATE_USER(setClause);
-            
-            const values = [...Object.values(updatedData), userId];
-            const result = await connection.query(sqlUpdate, values);
-            const updatedUser = result.rows[0];
+                const setClause = Object.keys(updatedData).map((key, index) => `${key} = $${index + 1}`).join(", ");
+                const sqlUpdate = userQueries.UPDATE_USER(setClause);
+                
+                const values = [...Object.values(updatedData), userId];
+                const result = await connection.query(sqlUpdate, values);
+                const updatedUser = result.rows[0];
 
-            return {
-                data: updatedUser,
-                message: `User with ID ${userId} has been successfully updated.`,
-            };
-        });
+                return {
+                    data: updatedUser,
+                    message: `User with ID ${userId} has been successfully updated.`,
+                };
+            });
+        } catch (error) {
+            console.error('Error updating user:', error);
+            throw new Error('Could not update user.');
+        }
     }
 }
